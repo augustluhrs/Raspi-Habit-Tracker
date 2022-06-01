44487543c71d0559a176bbbe4d1c29f778701814
Glitch (raspi-habit-tracker) <none>
2022-06-01 14:41:57 +0000

    🏤🦌 day sort and cleanup code

--- server.mjs
+++ server.mjs
@@ -3,12 +3,8 @@
   RASPI HABIT TRACKER
     ~ * ~ * ~ * 
 */
-import fetch from 'node-fetch';
-//const fetch = require('node-fetch');
-// const https = require('node:https');
 
-// mod.cjs
-//const fetch = () => import('node-fetch').then(({default: fetch}) => fetch());
+import fetch from 'node-fetch';
 
 const options = {
   hostname: '71.247.249.43',
@@ -19,11 +15,9 @@ const options = {
 
 //create express server
 let port = process.env.PORT || 8080;
-// let express = require('express');
 import express from 'express';
 import http from 'http';
 let app = express();
-//let server = require('http')
 let server = http.createServer(app).listen(port, function(){
   console.log('Server is listening at port: ', port);
 });
@@ -40,54 +34,96 @@ app.get('/getdays', function(req, res){
 });
 
 app.post('/updatedays', function(req, res){
-  // db.update({type: "recentDays", "recentDays.date": req.body.date}, { $push: {recentDays: req.body}}, {upsert: true}, function (err, newDoc){
-  let newDate = req.body.date;
   db.update({date: req.body.date}, { $set: {habits: req.body.newHabits}}, {upsert: true}, function (err, newDoc){
-    // console.log(req.body.date);
     if (err) {
       console.log ("insert err: " + err);
     } else {
-      //console.log("okay");
-      init();
-      
-      //send to raspi
-      sendToRaspi();
-      // fetch('71.247.249.43:8080/updateraspi', {
-      //   method: 'POST',
-      //   body: days,
-      // });
+      refresh();
     }
   });
   console.log(req.body); 
 });
 
 //nedb database stuff
-// const Datastore = require('nedb');
 import Datastore from 'nedb';
 let db = new Datastore({filename: "habits.db", autoload: true});
 
-//variables
+//days array that gets passed to and fro
 let days = [];
-let ready = false;
 
 //retrieve database for sketch
 init();
 function init() {
   db.find({}, (err, docs) => {
-    //console.log("docs: " + docs);
     if (err) {
       console.log("db err: " + err);
     }
     if (docs.length != 0) {
       days = docs;
-      ready = true; 
-      // console.log(days);
+      
+      //sort by date 
+      days.sort(compare);
     }
   })
 }
 
+//updates days and sends to raspi
+function refresh() {
+  db.find({}, (err, docs) => {
+    if (err) {
+      console.log("db err: " + err);
+    }
+    if (docs.length != 0) {
+      days = docs;
+      
+      //sort by date 
+      days.sort(compare);
+      
+      //send to raspi
+      const response = fetch('http://71.247.249.43:8080/updateraspi', {
+        method: 'POST', 
+        body: JSON.stringify(days),
+        headers: {'Content-Type': 'application/json'}
+      });
+      // const data = await response.json();
+      // console.log("response: " + data);
+    }
+  })
+}
 
-async function sendToRaspi(){
+//https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+function compare(a, b) {
+  if (a.date < b.date){
+    return -1;
+  }
+  if ( a.date > b.date ){
+    return 1;
+  }
+  return 0;
+}
+
+//clean up for testing
+// db.remove({}, {multi: true}, function (err, removed){
+//   if (err) {console.log("remove err: " + err);}
+//   if (removed){
+//     console.log ("removed:");
+//     console.log(removed);
+//   }
+// })
+
+//single entry removal
+// db.remove({date: "2022061"}, {multi: false}, function (err, removed){
+//   if (err) {console.log("remove err: " + err);}
+//   if (removed){
+//     console.log ("removed:");
+//     console.log(removed);
+//   }
+// });
+
+
+
+
+// async function sendToRaspi(){
   
 //   var options = {
 //   host: '71.247.249.43',
@@ -112,36 +148,14 @@ async function sendToRaspi(){
 //   req.write("data");
 //   req.end();
   
-  
-  
   // const response = await fetch('http://71.247.249.43:8080/test');
-  const response = await fetch('http://71.247.249.43:8080/updateraspi', {
-    method: 'POST', 
-    //body: days
-    body: JSON.stringify(days),
-    headers: {'Content-Type': 'application/json'}
-  });
-  const data = await response.json();
-  console.log("response: " + data);
-}
+  // const response = await fetch('http://71.247.249.43:8080/updateraspi', {
+  //   method: 'POST', 
+  //   //body: days
+  //   body: JSON.stringify(days),
+  //   headers: {'Content-Type': 'application/json'}
+  // });
+  // const data = await response.json();
+  // console.log("response: " + data);
+// }
 
-// db.update({type: "recentDays", recentDays: {[{date: 20220530, habits: []}]}}, {upsert: true}, function (err, newDoc){
-//   if (err) {
-//     console.log ("insert err: " + err);
-//   } else {
-//     console.log("okay");
-//   }
-// });
-
-// init();
-// console.log(days);
-
-
-//clean up for testing
-// db.remove({}, {multi: true}, function (err, removed){
-//   if (err) {console.log("remove err: " + err);}
-//   if (removed){
-//     console.log ("removed:");
-//     console.log(removed);
-//   }
-// })
